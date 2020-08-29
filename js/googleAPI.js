fc564d2ca0f05a16f8bec3e3193e8458d33ab195
SammyDeAgent <rimamotostheking@gmail.com>
2020-08-29 22:42:33 +0800

    Added async-promise js script

---
+++ js/googleAPI.js
@@ -0,0 +1,16 @@
+
+var gapiPromise = (function () {
+    var deferred = $.Deferred();
+    window.onLoadCallback = function () {
+        deferred.resolve(gapi);
+    };
+    return deferred.promise()
+}());
+
+var authInited = gapiPromise.then(function () {
+    gapi.auth2.init({
+        client_id: '505338635145-4r3f4cbq31r2nl60gfqjk7d473naarqb.apps.googleusercontent.com'
+    });
+})
+
+//gapiPromise.then(function(){ ... }) - function inside will be execueted when gapi is loaded
