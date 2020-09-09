c0ccce4d9b8a0449633f40e51e04c1659d6ad735
Jaxon <67098092+Yamam0o@users.noreply.github.com>
2020-09-09 16:37:57 +0800

    added live chat

--- js/cart.js
+++ js/cart.js
@@ -1,32 +1,46 @@
-$.getJSON("../JSON/fchicken.json", function (result) {});
-$.getJSON("../JSON/burger.json", function (result) {});
-$.getJSON("../JSON/coffee.json", function (result) {});
+// $.getJSON("../JSON/fchicken.json", function (result) {});
+// $.getJSON("../JSON/burger.json", function (result) {});
+// $.getJSON("../JSON/coffee.json", function (result) {});
 
-var temp = JSON.parse(localStorage.getItem("cartItem"));
+// var temp = JSON.parse(localStorage.getItem("cartItem"));
 
-if (temp == null) {
-    var food = [
-        { name: "chicken-1", price: 6, count: 0 },
-        { name: "chicken-2", price: 6, count: 0 },
-        { name: "chicken-3", price: 6, count: 0 },
-        { name: "chicken-4", price: 6, count: 0 },
-        { name: "burger-1", price: 6, count: 0 },
-        { name: "burger-2", price: 6, count: 0 },
-        { name: "burger-3", price: 6, count: 0 },
-        { name: "burger-4", price: 6, count: 0 },
-        { name: "coffee-1", price: 6, count: 0 },
-        { name: "coffee-2", price: 6, count: 0 },
-        { name: "coffee-3", price: 6, count: 0 },
-        { name: "coffee-4", price: 6, count: 0 },
-    ];
-} else {
-    var food = temp;
-}
+// if (temp == null) {
+//     var food = [
+//         { name: "chicken-1", price: 6, count: 0 },
+//         { name: "chicken-2", price: 6, count: 0 },
+//         { name: "chicken-3", price: 6, count: 0 },
+//         { name: "chicken-4", price: 6, count: 0 },
+//         { name: "burger-1", price: 6, count: 0 },
+//         { name: "burger-2", price: 6, count: 0 },
+//         { name: "burger-3", price: 6, count: 0 },
+//         { name: "burger-4", price: 6, count: 0 },
+//         { name: "coffee-1", price: 6, count: 0 },
+//         { name: "coffee-2", price: 6, count: 0 },
+//         { name: "coffee-3", price: 6, count: 0 },
+//         { name: "coffee-4", price: 6, count: 0 },
+//     ];
+// } else {
+//     var food = temp;
+// }
 
 $(function () {
-    $(".add-cart").click(function () {
-        var id = parseInt($(this).attr("data-id"));
-        food[id].count++;
-        localStorage.setItem("cartItem", JSON.stringify(food));
+    $("button").on("click", function () {
+        var id = $(this).attr("data-id");
+
+        let cat = parseInt(id[0]);
+
+        switch (cat) {
+            case 0:
+                $.getJSON("../JSON/fchicken.json", (result) =>
+                    cardAppend(result, "cat1", i)
+                );
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+
+        console.log(cat);
     });
 });
