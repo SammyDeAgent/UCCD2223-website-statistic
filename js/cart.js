914db2fe34e302f4ba9fbb934acb4acbe8f055cc
SammyDeAgent <rimamotostheking@gmail.com>
2020-09-09 23:37:52 +0800

    Hot Fix #5

--- js/cart.js
+++ js/cart.js
@@ -1,32 +1,37 @@
 $(document).ready(function () {
     //Adding To Cart
-    $("#cardMenuWrapper").on("click", "button", () => {
-        updateCount($(this)[0].activeElement.attributes["data-id"].nodeValue);
-    });
-});
+    function onSignIn(googleUser) {
 
-function updateCount(id) {
-    gapiPromise.then(() => {
-        var profile = gapi.auth2.currentUser.get().getBasicProfile();
-    })
-    var locate = false;
-    var record = localStorage.getItem(profile.getId());
-    if (record == null) {
-        record = [];
-    }
+        var profile = googleUser.getBasicProfile();
 
-    for (let i = 0; i < record.length; i++) {
-        if (record[i][0] == id) {
-            record[i][1].count++;
-            locate = true;
+        $("#cardMenuWrapper").on("click", "button", () => {
+            updateCount($(this)[0].activeElement.attributes["data-id"].nodeValue);
+        });
+
+        function updateCount(id) {
+            var locate = false;
+            var record = localStorage.getItem(profile.getId());
+            if (record == null) {
+                record = [];
+            }
+
+            for (let i = 0; i < record.length; i++) {
+                if (record[i][0] == id) {
+                    record[i][1].count++;
+                    locate = true;
+                }
+            }
+            if (!locate) {
+                record.push([id, 1]);
+            }
+
+            localStorage.setItem(profile.getId(), record);
         }
     }
-    if (!locate) {
-        record.push([id, 1]);
-    }
 
-    localStorage.setItem(profile.getId(), record);
-}
+});
+
+
 
 // function setCount() {
 //     let i = parseInt(localStorage.getItem("tCount"));
