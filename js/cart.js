a06ab1f063d7c9d8a6f635e27ed7c38bdbff07fd
Jaxon <67098092+Yamam0o@users.noreply.github.com>
2020-09-09 20:55:34 +0800

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,6 @@
-
-$(document).ready(function(){
-
+$(document).ready(function () {
     //Adding To Cart
-    $("#cardMenuWrapper").on('click','button',()=>{
+    $("#cardMenuWrapper").on("click", "button", () => {
         console.log($(this)[0].activeElement.attributes["data-id"].nodeValue);
     });
-})
+});
