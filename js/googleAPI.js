5e673bfd23a8645c5c6e17059a051690700c7594
Sammy <66596766+SammyDeAgent@users.noreply.github.com>
2021-12-12 21:02:19 +0800

    Update googleAPI.js

--- js/googleAPI.js
+++ js/googleAPI.js
@@ -8,7 +8,7 @@ var gapiPromise = (function () {
 
 var authInited = gapiPromise.then(function () {
     gapi.auth2.init({
-        client_id: '505338635145-4r3f4cbq31r2nl60gfqjk7d473naarqb.apps.googleusercontent.com'
+        client_id: '505338635145-8fgfrtunnk4r8f673oc21s6blp10e774.apps.googleusercontent.com'
     });
 })
 
